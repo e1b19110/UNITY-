e7c5ae50bd082dcb5fbb7bf28be5c84ed08ebe6d
e1b19110 <75967519+e1b19110@users.noreply.github.com>
2022-02-06 19:23:55 +0900

    魔女スクリプト(第一章)

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,43 +1,71 @@
-﻿using System.Collections;
+uusing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
-public class PlayerController : MonoBehaviour
+public class majo : MonoBehaviour
 {
-    Rigidbody2D rbody;          // Rigidbody2D 型の変数
-    float axisH = 0.0f;         // 入力
-    public float speed = 3.0f;  // 移動速度
 
-    // Start is called before the first frame update
+    Rigidbody2D rbody;// Rigidbody2D 型の変数(majo)
+    float Bottun = 0.0f;             // 入力
+    public float speed = 6.0f;      // 移動速度
+
+
+
+   
+
+
+    //ゲーム開始時に一度だけ行う処理
     void Start()
     {
         // Rigidbody2Dを取ってくる
         rbody = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
+
+
+
+    //フレームごとに行う関数(時間間隔は不均一)
     void Update()
     {
+
         // 水平方向の入力をチェックする
-        axisH = Input.GetAxisRaw("Horizontal");
+        Bottun = Input.GetAxisRaw("Horizontal");
         // 向きの調整
-        if(axisH > 0.0f)
+        if (Bottun > 0.0f)
         {
-            // 右移動
-            Debug.Log("右移動");
-            transform.localScale = new Vector2(1,1);
+
+            transform.localScale = new Vector2(1, 1);
         }
-        else if (axisH < 0.0f)
+        else if (Bottun < 0.0f)
         {
-            // 左移動
-            Debug.Log("左移動");
+
             transform.localScale = new Vector2(-1, 1);   // 左右反転させる
         }
+
+
     }
 
+    //フレームごとに行う関数(時間間隔は均一)
     void FixedUpdate()
     {
-        // 速度を更新する
-        rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y);
+
+
+        if (Bottun != 0)
+        {
+
+            // 速度を更新する
+            rbody.velocity = new Vector2(speed * Bottun, rbody.velocity.y);
+        }
+       
     }
+
+
+
+    
+    
 }
+
+
+
